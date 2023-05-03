03cc2a99637bd91c3c1d00c924329ac0fbfc54c2
Laura Dobie <moving.toyshop@gmail.com>
2023-05-03 22:55:28 +0100

    Set up Hello Sage app with controller and hello endpoint

---
+++ src/main/java/hello/world/maven/web/HelloWorldController.java
@@ -0,0 +1,15 @@
+package hello.world.maven.web;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/hello")
+public class HelloWorldController {
+    @Get("/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloMicronaut() {
+        return "Hello, Sage!";
+    }
+}
